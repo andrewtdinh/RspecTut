f16eba0ff333d035910f7149a602fb3bbce4c55d
Andrew Dinh <andrewdinh@breakthrough.com>
2015-12-14 02:47:18 -0800

    Refactor the Cart class

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,9 +1,11 @@
 class Cart
+  extend Forwardable
+  def_delegator :@items, :empty?
   def initialize
     @items = {}
   end
 
-  def empty?
-    @items.empty?
-  end
+  # def empty?
+  #   @items.empty?
+  # end
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,3 +1,14 @@
-describe Cart do
+require '../lib/cart.rb'
 
+describe 'An instant of', Cart do
+
+  before :each do
+    @cart = Cart.new
+  end
+
+  context 'when new' do
+    it 'contains no items' do
+      expect(@cart).to be_empty
+    end
+  end
 end
