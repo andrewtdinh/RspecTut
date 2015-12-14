d7e3bfad1815eb2c860cbefab0ccaa0df215d5f7
Andrew Dinh <andrewdinh@breakthrough.com>
2015-12-14 02:17:41 -0800

    Add code to run before each test

--- rspec/cart_spec.rb
+++ rspec/cart_spec.rb
@@ -1,6 +1,11 @@
 require '../lib/cart.rb'
 
 describe 'An instant of', Cart do
+
+  before :each do
+    @cart = Cart.new
+  end
+
   context 'when new' do
     it 'contains no items' do
       expect(@cart).to be_empty
